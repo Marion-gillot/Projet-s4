02320589fd9f6344c9d505c43d1d60cbdab19ada
Marion-gillot <marion.gillot@edu.univ-fcomte.fr>
2023-02-02 11:47:57 +0100

    test d'animation : text progressif

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,29 @@ $('.card').on('mousemove', function(e) {
       transform: 'translate(-' + x / 20 + '%, -' + y / 20 + '%)'
     });
   });
+
+
+/*-----
+Spanizer
+- Wraps letters with spans, for css animations
+-----*/
+(function($) {
+  var s,
+  spanizeLetters = {
+    settings: {
+      letters: $('.js-spanize'),
+    },
+    init: function() {
+      s = this.settings;
+      this.bindEvents();
+    },
+    bindEvents: function(){
+      s.letters.html(function (i, el) {
+        //spanizeLetters.joinChars();
+        var spanizer = $.trim(el).split("");
+        return '<span>' + spanizer.join('</span><span>') + '</span>';
+      });
+    },
+  };
+  spanizeLetters.init();
+})(jQuery);
